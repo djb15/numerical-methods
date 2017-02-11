b8c2ba21c84f36a7fab28abe82c8e9a1f20d0e14
djb15 <dirk.judd@live.com>
2017-02-11 11:19:42 +0000

    feat: Final ralston and script

--- ralston.m
+++ ralston.m
@@ -1,6 +1,12 @@
 function [t,vout] = ralston(func,i0,vin,tf,R,L,h)
-    %UNTITLED2 Summary of this function goes here
-    %   Detailed explanation goes here 
+    %Performs the ralston numerical method (output t,vout)
+    %   func = ODE
+    %   i0 = Initial condition
+    %   vin = Prebuilt array with input voltage waveform
+    %   tf = Final time
+    %   R = Resistor value
+    %   L = Inductor value
+    %   h = step size
 
     N=round(tf/h); % set size of arrays, nr of steps
 
@@ -8,17 +14,17 @@ function [t,vout] = ralston(func,i0,vin,tf,R,L,h)
     t=zeros(1,N);
     vout=zeros(1,N);
 
-    i(1)=i0; % initialize arrays 
+    i(1)=i0; % initial condition setup 
 
     for j=1:N-1 % loop for N steps
        
-        k1 = feval(func,vin(j),i(j),R,L); %evaluate function
+        k1 = feval(func,vin(j),i(j),R,L); %evaluate function for k1 and k2
         k2 = feval(func,vin(j)+(2/3)*h,i(j)+(2/3)*h*k1,R,L);
 
         i(j+1)= i(j) + h*(0.25*k1 + 0.75*k2); % next value of i calculated
         t(j+1)=t(j)+h; % next value of t, increase by h
 
-        vout(j) = vin(j) - i(j)*R;
+        vout(j) = vin(j) - i(j)*R; %returns vout from vin and estimated current
     end
 end
 
