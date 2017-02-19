3ca7bce86f3f8901ce268e401aa0cb5d945d11cf
djb15 <dirk.judd@live.com>
2017-02-19 21:38:43 +0000

    feat: Fully working finite script

--- finite_script.m
+++ finite_script.m
@@ -4,16 +4,23 @@ N = round(1/h);
 
 U1 = zeros(N+1);
 U2 = zeros(N+1);
+U3 = zeros(N+1);
+U4 = zeros(N+1);
+U5 = zeros(N+1);
 
 divs = 0:h:1;
 y0 = (sawtooth(2*pi*divs,0.5)+1)/2;
 y1 = sin(2*pi*divs);
 y2 = abs(sin(2*pi*divs));
+y3 = square(2*pi*divs);
+y4 = sawtooth(2*pi*divs);
 
 
-U1(1,1:N+1) = y0;
-U2(1,1:N+1) = y1;
-U3(1,1:N+1) = y2;
+U1(1,2:N) = y0(2:N);
+U2(1,2:N) = y1(2:N);
+U3(1,2:N) = y2(2:N);
+U4(1,2:N) = y3(2:N);
+U5(1,2:N) = y4(2:N);
 
 %At x=0 and x=N (i.e. the start and end of the rod) the value is 0 for all
 %m.  This corresponds to the first and last column of the matrix being 0.
@@ -26,22 +33,38 @@ for m=2:1502
         U1(m,j) = v*U1(m-1,j-1) + b*U1(m-1,j) + v*U1(m-1, j+1);
         U2(m,j) = v*U2(m-1,j-1) + b*U2(m-1,j) + v*U2(m-1, j+1);
         U3(m,j) = v*U3(m-1,j-1) + b*U3(m-1,j) + v*U3(m-1, j+1);
+        U4(m,j) = v*U4(m-1,j-1) + b*U4(m-1,j) + v*U4(m-1, j+1);
+        U5(m,j) = v*U5(m-1,j-1) + b*U5(m-1,j) + v*U5(m-1, j+1);
     end
 end
 
 step = 1:150:1501;
-figure(1);
-figure(2);
-figure(3);
+figure;
 
 for i=step
-    figure(1);
+    subplot(2,3,1);
     plot(divs,U1(i,1:N+1));
+    title('Triangle wave initial condition');
+    xlabel('Distance');
     hold on;
-    figure(2);
+    subplot(2,3,2);
     plot(divs,U2(i,1:N+1));
+    title('Sine wave initial condition');
+    xlabel('Distance');
     hold on;
-    figure(3);
+    subplot(2,3,3);
     plot(divs,U3(i,1:N+1));
+    title('|Sine| wave initial condition');
+    xlabel('Distance');
+    hold on;
+    subplot(2,3,4);
+    plot(divs,U4(i,1:N+1));
+    title('Square wave initial condition');
+    xlabel('Distance');
+    hold on;
+    subplot(2,3,5);
+    plot(divs,U5(i,1:N+1));
+    title('Sawtooth wave initial condition');
+    xlabel('Distance');
     hold on;
 end
