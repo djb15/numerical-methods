ac5f287b1e33a542819e88d40bc46a7fd55cebe1
djb15 <dirk.judd@live.com>
2017-03-04 12:12:53 +0000

    feat: Basis for bonus exercises 2 and 3

--- finite_script.m
+++ finite_script.m
@@ -7,6 +7,8 @@ U2 = zeros(N+1);
 U3 = zeros(N+1);
 U4 = zeros(N+1);
 U5 = zeros(N+1);
+U6 = zeros(N+1);
+U7 = zeros(N+1);
 
 divs = 0:h:1;
 y0 = (sawtooth(2*pi*divs,0.5)+1)/2;
@@ -21,20 +23,42 @@ U2(1,2:N) = y1(2:N);
 U3(1,2:N) = y2(2:N);
 U4(1,2:N) = y3(2:N);
 U5(1,2:N) = y4(2:N);
+U6(1,2:N) = y0(2:N);
+U7(1,2:N) = y0(2:N);
 
 %At x=0 and x=N (i.e. the start and end of the rod) the value is 0 for all
 %m.  This corresponds to the first and last column of the matrix being 0.
 v = k/h^2;
 b = 1-2*v;
 
+% Set non-zero constant boundary conditions here
+low_bound = 2;
+high_bound = 0;
+
+U6(1:N+1,1) = low_bound;
+U6(1:N+1,N+1) = high_bound;
+
+% Set time varying boundary conditions here
+low_bound = @(time) 100*exp(-(2+50*time));
+high_bound = @(time) 100*exp(-(2+50*time));
+
+U7(1,1) = feval(low_bound, 0);
+U7(1,N+1) = feval(high_bound, 0);
+
+
 
 for m=2:1502
+    % Time varying boundary conditions set here also
+    U7(m,1) = feval(low_bound, (m-1)*k);
+    U7(m,N+1) = feval(high_bound, (m-1)*k);
+    
     for j=2:N
         U1(m,j) = v*U1(m-1,j-1) + b*U1(m-1,j) + v*U1(m-1, j+1);
         U2(m,j) = v*U2(m-1,j-1) + b*U2(m-1,j) + v*U2(m-1, j+1);
         U3(m,j) = v*U3(m-1,j-1) + b*U3(m-1,j) + v*U3(m-1, j+1);
         U4(m,j) = v*U4(m-1,j-1) + b*U4(m-1,j) + v*U4(m-1, j+1);
         U5(m,j) = v*U5(m-1,j-1) + b*U5(m-1,j) + v*U5(m-1, j+1);
+        U6(m,j) = v*U6(m-1,j-1) + b*U6(m-1,j) + v*U6(m-1, j+1);
     end
 end
 
@@ -65,6 +89,11 @@ for i=step
     subplot(2,3,5);
     plot(divs,U5(i,1:N+1));
     title('Sawtooth wave initial condition');
+    xlabel('Distance');
+    hold on;
+    subplot(2,3,6);
+    plot(divs,U6(i,1:N+1));
+    title('Triangle wave with non-zero constant boundary conditions');
     xlabel('Distance');
     hold on;
 end
