38f6483b697c50a71ef71c582c6bc81396aabd78
djb15 <dirk.judd@live.com>
2017-03-06 23:59:00 +0000

    feat: Fixed heun script

--- heun.m
+++ heun.m
@@ -13,12 +13,13 @@ t = zeros(1,N);
 i(1) = i0; % set initial value of i
 
 for x = 1:N-1 % loop for N-1 steps
+    t(x+1)=t(x)+h; %step by h for next value of t
     ip = i(x) + h*feval(func, t(x), i(x), vin); % predictor for next value of i
-    grad1=feval(func, t(x), ip, vin); % gradient at t+h
+    grad1=feval(func, t(x+1), ip, vin); % gradient at t+h
     grad2=feval(func, t(x), i(x), vin); % gradient at t
     iave=0.5*(grad1+grad2); % average gradient over [t,t+h]
     i(x+1)=i(x)+h*iave; % new i from euler, using iave as gradient
-    t(x+1)=t(x)+h; %step by h for next value of t
+
     
 
 end
